a0ec8ea0c3c7f528d816a757230fbb8b41dc7b0c
Leonardo Oliveira <leoo2771@gmail.com>
2025-01-16 10:29:05 -0300

    correção skillsLanguage

--- app/SkillLangueage/page.tsx
+++ app/SkillLangueage/page.tsx
@@ -1,51 +1,14 @@
-import React from "react";
+import SkillsLanguage from './components/SkillsLanguage'
 
-interface Props {
-    skill1: string
-    skill2: string
-    skill3: string
-    level1: string
-    level2: string
-    level3: string
-}
-
-const SkillsLanguage = ({
-    level1,
-    level2,
-    level3,
-    skill1,
-    skill2,
-    skill3
-}: Props) => {
+export default function SkillLanguagePage() {
     return (
-        <div>
-            <div className="relative mb-[3rem]">
-                <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-                    {skill1}
-                </h1>
-                <span
-                    className={`${level1} bottom-0 h-[6px] absolute bg-[#55e6a5]`}>
-                </span>
-            </div>
-            <div className="relative mb-[3rem]">
-                <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-                    {skill2}
-                </h1>
-                <span
-                    className={`${level3} bottom-0 h-[6px] absolute bg-[#55e6a5]`}>
-                </span>
-            </div>
-
-            <div className="relative mb-[3rem]">
-                <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-                    {skill3}
-                </h1>
-                <span
-                    className={`${level3} bottom-0 h-[6px] absolute bg-[#55e6a5]`}>
-                </span>
-            </div>
-        </div>
+        <SkillsLanguage 
+            skill1="Skill 1"
+            skill2="Skill 2"
+            skill3="Skill 3"
+            level1="w-[90%]"
+            level2="w-[80%]"
+            level3="w-[70%]"
+        />
     )
 }
-
-export default SkillsLanguage
